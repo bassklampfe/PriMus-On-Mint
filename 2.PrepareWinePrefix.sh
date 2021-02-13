dbc9e4ae6d0ce917d7faf723d1734abef7e3dba1
Jørgen <bassklampfe@users.noreply.github.com>
2021-02-13 10:20:45 +0100

    Since microsoft has removed the download and winetricks then falls back to the very slow internet archive we will prefetch the huge kb files from faster source

--- 2.PrepareWinePrefix.sh
+++ 2.PrepareWinePrefix.sh
@@ -6,8 +6,20 @@ set -e
 # PriMus from Columbussoft on Linux using wine
 #------------------------------------------------------------
 
+# Since microsoft has removed the download and 
+# winetricks then falls back to the very slow internet archive
+# we will prefetch the huge kb files from faster source
+for kb in windows6.1-KB976932-X86.exe windows6.1-KB976932-X64.exe
+do
+	if [ ! -f ${HOME}/.cache/winetricks/win7sp1/${kb} ]
+	then
+		mkdir -p ${HOME}/.cache/winetricks/win7sp1/
+		wget http://www-pc.uni-regensburg.de/systemsw/win7/sp1/${kb} \
+		-O ${HOME}/.cache/winetricks/win7sp1/${kb}
+	fi
+done
 export WINEPREFIX="${HOME}/.wine-primus"
 rm -rf "${WINEPREFIX}"
 # setup wine environment (will recreate dir)
-winetricks corefonts gdiplus vcrun2005
+winetricks --unattended corefonts lucida gdiplus vcrun2005
 

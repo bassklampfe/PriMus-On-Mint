16180ec417ea24c0801029d6ad2fc5c18fd2d5ee
J.Jørgen von Bargen <bassklampfe@users.noreply.github.com>
2024-12-02 08:10:52 +0100

    on print: dont copy to temp, but chdir to dir of primus file, else relative references to pictures wont resolve

--- bin/primus2pdf.sh
+++ bin/primus2pdf.sh
@@ -16,18 +16,18 @@ function pri2pdf
 		echo "======= ${pri_path} ======"
 		if [ -f "${pri_path}" ]
 		then
-			#
-			# copy primus file into temp folder (to avoid issues with special chars in paths)
-			#
-			pri=$(basename "${pri_path}")
-			#pri=${pri//[^[:alnum:].]/_}
-			echo "pri='${pri}'"
-			cp "${pri_path}" "/tmp/${pri}"
-
+			real_path=$(realpath "${pri_path}")
+			echo "real_path='${real_path}'"
+			real_dir=$(dirname "${real_path}")
+			echo "real_dir='${real_dir}'"
+			real_name=$(basename "${real_path}")
+			echo "real_name='${real_name}'"
+	
+			pdf=${real_path%.pri}.pdf
 			#
 			# determ expected output name of PDF (depending on switch in primus)
 			#
-			prinam=$(echo -n ${pri%.pri}|iconv -f UTF-8 -t ISO-8859-14 - |perl -pe 's/[^.a-zA-Z0-9\x80-\xFF+-]/_/g;s/([\x80-\xFF])/sprintf("_%03o",ord($1))/ge;')
+			prinam=$(echo -n ${real_name%.pri}|iconv -f UTF-8 -t ISO-8859-14 - |perl -pe 's/[^.a-zA-Z0-9\x80-\xFF+-]/_/g;s/([\x80-\xFF])/sprintf("_%03o",ord($1))/ge;')
 			echo "prinam ='${prinam}'"
 			
 			nam1=${prinam:0:64} ; nam1="$(echo -n ${nam1}| perl -pe 's/_+$//')"
@@ -39,17 +39,19 @@ function pri2pdf
 			#
 			# do the print
 			#
-			echo "Print '${pri}' => '${nam}'  => '${pdf}'"
+			echo "Print '${real_name}' => '${prinam}'  => '${pdf}'"
 			rm -f "${HOME}/PDF/${nam1}"*.pdf "${HOME}/PDF/${nam2}"*.pdf
-			wine 'C:\Program Files (x86)\PriMus\PriMus.exe' -PRN "PDF" -P "z:/tmp/${pri}"
-			#env WINEPREFIX="${HOME}/.wine" wine 'C:\Program Files (x86)\PriMus\PriMus.exe' -PRN "PDF" -P z:"/tmp/${pri}" || true
+			(
+				cd "${real_dir}"
+				wine 'C:\Program Files (x86)\PriMus\PriMus.exe' -PRN "PDF" -P "${real_name}"
+			)
 			echo "Printed status $?"
 			sleep 1
 			#
 			# wanted file names in folder of primus document
 			#
-			pdf=${pri_path%.pri}.pdf
-			oldpdf=${pri_path%.pri}-old.pdf
+			pdf=${real_path%.pri}.pdf
+			oldpdf=${real_path%.pri}-old.pdf
 			pdfpdf=$(find ${HOME}/PDF/ -name "${nam1}*.pdf" -or -name "${nam2}*.pdf")
 			echo "pdfpdf='${pdfpdf}'"
 
@@ -57,7 +59,7 @@ function pri2pdf
 			# rename files to targed
 			#
 			test -f "${pdf}"    && mv "${pdf}" "${oldpdf}"
-			test -f "${pdfpdf}" && mv -v "${pdfpdf}" "${pdf}"  && touch --no-create --reference "${pri_path}"  "${pdf}"
+			test -f "${pdfpdf}" && mv -v "${pdfpdf}" "${pdf}"  && touch --no-create --reference "${real_path}"  "${pdf}"
 		fi
 	done
 	echo "*** ALL DONE ***"
